21515e7887baf0b0bcf9c476d85db6a3d8617ac5
Carlos Ribeiro <carlos.ribeiro.537h@gmail.com>
2017-12-03 09:00:48 -0200

    Colored text

---
+++ ConsoleUI/Text.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI {
+    public class Text {
+        public static void PrintInColor(string text, ConsoleColor textColor = ConsoleColor.Green, ConsoleColor backgroundColor = ConsoleColor.Black) {
+            ConsoleColor tmpTextColor = Console.ForegroundColor;
+            ConsoleColor tmpBackColor = Console.BackgroundColor;
+            Console.ForegroundColor = textColor;
+            Console.BackgroundColor = backgroundColor;
+            Console.Write(text);
+            Console.ForegroundColor = tmpTextColor;
+            Console.BackgroundColor = tmpBackColor;
+        }
+    }
+}
